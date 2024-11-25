e2567a47cff2aa147fb1998d33e49683eb701e30
sreiswig <sam.reiswig@gmail.com>
2024-11-24 23:11:31 -0600

    test that deck is initialized correctly

--- src/tests/test_liars_game.py
+++ src/tests/test_liars_game.py
@@ -4,4 +4,12 @@ from simulator import Player
 def test_initialize_game():
     players = [Player(), Player(), Player(), Player()]
     game = LiarsGame(players)
-    assert len(game.players) == 4
+    face_counts = {"King": 0, "Queen": 0, "Jack": 0, "Ace": 0, "Joker": 0}
+    for card in game.deck.deck:
+        face_counts[card.face] = face_counts[card.face] + 1
+
+    assert face_counts["King"] == 6
+    assert face_counts["Queen"] == 6
+    assert face_counts["Jack"] == 6
+    assert face_counts["Ace"] == 6
+    assert face_counts["Joker"] == 2 
